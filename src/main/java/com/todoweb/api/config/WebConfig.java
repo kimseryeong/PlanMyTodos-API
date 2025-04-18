3b028f3bca9d4869f3afc17d29b562e12f40e40b
KimSR <kxxoo322@gmail.com>
2025-04-18 16:28:19 +0900

    settings: CORS 설정

---
+++ src/main/java/com/todoweb/api/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.todoweb.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("https://planmytodos.netlify.app")
+			.allowedOrigins("http://localhost:3000")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("Authorization", "Content-Type")
+			.allowCredentials(true);
+	}
+}
