ef49f17075c404ba28da96f88e60fca3f8f6f957
KimSR <kxxoo322@gmail.com>
2025-04-23 14:27:24 +0900

    fix: oauth2Login 설정 수정

--- src/main/java/com/todoweb/api/common/config/SecurityConfig.java
+++ src/main/java/com/todoweb/api/common/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,8 +33,7 @@ public class SecurityConfig {
 	        .authorizeHttpRequests(requests -> requests
         		.requestMatchers("/", "/user/**", "/oauth2/**").permitAll()
 	            .anyRequest().permitAll())
-	        .oauth2Login(oauth2 -> oauth2
-	            .loginPage("/login")); // 사용자 정의 로그인 페이지 경로 (선택 사항)
+	        .oauth2Login(Customizer.withDefaults()); // 사용자 정의 로그인 페이지 경로 (선택 사항)
 		
 	    return http.build();
 //		http
