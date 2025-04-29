6429533a9b36446693aedde511defdd46d5281e8
KimSR <kxxoo322@gmail.com>
2025-04-29 13:28:03 +0900

    fix: root path return 변경

--- src/main/java/com/todoweb/api/controller/RootController.java
+++ src/main/java/com/todoweb/api/controller/RootController.java
@@ -1,5 +1,9 @@
 package com.todoweb.api.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
 
 	@GetMapping("/")
-    public String root() {
-        return "Server is up and running!";
+    public ResponseEntity<?> root() {
+		Map<String, Object> response = new HashMap<>();
+		
+		response.put("status", 200);
+		response.put("message", "This is Root Path");
+		
+		return ResponseEntity.ok().body(response);
     }
 }
