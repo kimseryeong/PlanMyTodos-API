840c4a0560884ed5d886e076d33871445c7176e9
KimSR <kxxoo322@gmail.com>
2025-04-29 13:55:43 +0900

    fix: root 경로 return 수정

--- src/main/java/com/todoweb/api/controller/RootController.java
+++ src/main/java/com/todoweb/api/controller/RootController.java
@@ -1,9 +1,5 @@
 package com.todoweb.api.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
 
 	@GetMapping("/")
-    public ResponseEntity<?> root() {
-		Map<String, Object> response = new HashMap<>();
-		
-		response.put("status", 200);
-		response.put("message", "This is Root Path");
-		
-		return ResponseEntity.ok().body(response);
+    public String root() {
+		return "Root";
     }
 }
