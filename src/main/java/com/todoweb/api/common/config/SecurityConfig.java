226aaf239e95b8ea0dae6b88143dacb21fca692d
KimSR <kxxoo322@gmail.com>
2025-04-29 09:08:22 +0900

    fix: corsconfigure headers 추가

--- src/main/java/com/todoweb/api/common/config/SecurityConfig.java
+++ src/main/java/com/todoweb/api/common/config/SecurityConfig.java
@@ -50,9 +50,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         
-        configuration.setAllowedOrigins(List.of("https://planmytodos.netlify.app", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://planmytodos.netlify.app/", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-XSRF-TOKEN"));
         configuration.setAllowCredentials(true);
         
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
